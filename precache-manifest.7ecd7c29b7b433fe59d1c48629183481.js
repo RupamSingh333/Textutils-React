7256d360c75accbaa86de11ceac4a27a76309355
RupamSingh333 <rupamkumar333@gmail.com.com>
2022-04-03 22:14:05 +0530

    Updates

---
+++ precache-manifest.7ecd7c29b7b433fe59d1c48629183481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56b4d945276bede89b57",
+    "url": "/Textutils-React/static/css/main.ea6081c5.chunk.css"
+  },
+  {
+    "revision": "56b4d945276bede89b57",
+    "url": "/Textutils-React/static/js/main.56b4d945.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/Textutils-React/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/Textutils-React/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "a479dcaf9ae7d98d982f",
+    "url": "/Textutils-React/static/js/runtime~main.a479dcaf.js"
+  },
+  {
+    "revision": "9a018ac77dfbf424880ecd4a92e85801",
+    "url": "/Textutils-React/index.html"
+  }
+];
